e110b218a2178a605f25cf69a2ac7675ad06ae57
sumeshkanayi <sumeshkanayi@gmail.com>
2017-06-16 12:49:48 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -2,7 +2,7 @@ source_httpd="/vagrant/mspidea1zips/mspidea/httpd"
 source_apache2="/vagrant/mspidea1zips/mspidea/apache2"
 mv  /etc/httpd/conf /etc/httdpd/conf_backup
 cp -r "${source_httpd}/conf" /etc/httpd
-cp -r "${source_httpd}/sites-*" /etc/httpd
+cp -r "${source_httpd}/sites-enabled" /etc/httpd
 cp -r "${source_apache2}" /etc/
 if [ -d "/var/www/html" ]; then
 mkdir -p /var/www/html
