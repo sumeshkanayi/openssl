f0b81d955a4f505333eb50e5e9d3541f1be477d9
sumeshkanayi <sumeshkanayi@gmail.com>
2017-06-16 16:44:48 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,6 +1,7 @@
-source_httpd="/vagrant/mspidea1zips/mspidea/httpd"
-source_apache2="/vagrant/mspidea1zips/mspidea/apache2"
-var_www="/vagrant/www/www"
+source_httpd="/build/httpd"
+source_apache2="/build/apache2"
+var_www="/build/www"
+subversion_conf="/build/subversion"
 mv  /etc/httpd/conf /etc/httpd/conf_backup
 cp -r "${source_httpd}/conf" /etc/httpd
 cp -r "${var_www}" /var
@@ -8,12 +9,8 @@ cp -r "${source_httpd}/sites-enabled" /etc/httpd
 cp -r "${source_apache2}" /etc/
 cp -r "${source_httpd}/conf.modules.d" /etc/httpd
 cp -r "${source_httpd}/conf.d" /etc/httpd
-#if [ ! -e "/var/www/html" ]; then
-#mkdir -p /var/www/html
-#echo "<p>"Hello world"</p>" > /var/www/html/index.html
-#echo "<p>"Hello world "</p>" > /var/www/index.html
-#
-#fi
+cp ${subversion_conf}/*.* /etc/subversion
+
 groupadd -G apache
 sudo useradd -d /var/www/html -g apache apache
 mkdir -p /var/log/apache2
