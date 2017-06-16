36f5dfceda7a79306abcaf2433b8fd93005b5076
sumeshkanayi <sumeshkanayi@gmail.com>
2017-06-16 12:45:08 +0530

    Create post.sh

---
+++ post.sh
@@ -0,0 +1,9 @@
+source_httpd="/vagrant/mspidea1zips/mspidea/httpd'
+source_apache2="/vagrant/mspidea1zips/mspidea/apache2'
+mv  /etc/httpd/conf /etc/httdpd/conf_backup
+cp -r "${source_httpd}/conf" /etc/httpd
+cp -r "${source_httpd}/sites-*" /etc/httpd
+cp -r "${source_apache2}" /etc/
+
+
+
