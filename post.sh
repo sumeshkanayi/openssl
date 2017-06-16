a6caae1e433658b50dd4a647fa93a23b7414a02c
sumeshkanayi <sumeshkanayi@gmail.com>
2017-06-16 13:41:08 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,17 +1,19 @@
 source_httpd="/vagrant/mspidea1zips/mspidea/httpd"
 source_apache2="/vagrant/mspidea1zips/mspidea/apache2"
+var_www="/vagrant/www"
 mv  /etc/httpd/conf /etc/httpd/conf_backup
 cp -r "${source_httpd}/conf" /etc/httpd
+cp -r "${var_www}" /var
 cp -r "${source_httpd}/sites-enabled" /etc/httpd
 cp -r "${source_apache2}" /etc/
 cp -r "${source_httpd}/conf.modules.d" /etc/httpd
 cp -r "${source_httpd}/conf.d" /etc/httpd
-if [ ! -e "/var/www/html" ]; then
-mkdir -p /var/www/html
-echo "<p>"Hello world"</p>" > /var/www/html/index.html
-echo "<p>"Hello world "</p>" > /var/www/index.html
-
-fi
+#if [ ! -e "/var/www/html" ]; then
+#mkdir -p /var/www/html
+#echo "<p>"Hello world"</p>" > /var/www/html/index.html
+#echo "<p>"Hello world "</p>" > /var/www/index.html
+#
+#fi
 groupadd -G apache
 sudo useradd -d /var/www/html -g apache apache
 mkdir -p /var/log/apache2
