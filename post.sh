7337508aced589666697dd84da9229ae58f062bd
sumeshkanayi <sumeshkanayi@gmail.com>
2017-06-16 12:50:14 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,6 +1,6 @@
 source_httpd="/vagrant/mspidea1zips/mspidea/httpd"
 source_apache2="/vagrant/mspidea1zips/mspidea/apache2"
-mv  /etc/httpd/conf /etc/httdpd/conf_backup
+mv  /etc/httpd/conf /etc/httpd/conf_backup
 cp -r "${source_httpd}/conf" /etc/httpd
 cp -r "${source_httpd}/sites-enabled" /etc/httpd
 cp -r "${source_apache2}" /etc/
