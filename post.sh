0ef8b1b8499911b70bcc10e77252c2186524de98
sumeshkanayi <sumeshkanayi@gmail.com>
2017-06-16 13:06:52 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -16,6 +16,7 @@ fi
 
 mv /etc/httpd/sites-enabled/svn.conf /etc/httpd/sites-enabled/svn.conf_backup
 mv /etc/httpd/conf.modules.d/10-subversion.conf /etc/httpd/conf.modules.d/10-subversion.conf_backup
+mv /etc/httpd/conf.modules.d/00-systemd.conf /etc/httpd/conf.modules.d/00-systemd.conf_backup
 
 
 
