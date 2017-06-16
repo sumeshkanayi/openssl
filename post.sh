b4eae609409850e10213448f9a8295c9bccd3c8d
sumeshkanayi <sumeshkanayi@gmail.com>
2017-06-16 12:54:13 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -4,7 +4,8 @@ mv  /etc/httpd/conf /etc/httpd/conf_backup
 cp -r "${source_httpd}/conf" /etc/httpd
 cp -r "${source_httpd}/sites-enabled" /etc/httpd
 cp -r "${source_apache2}" /etc/
-if [ -d "/var/www/html" ]; then
+cp -r "${source_httpd}/conf.modules.d" /etc/httpd
+if [ ! -e "/var/www/html" ]; then
 mkdir -p /var/www/html
 echo "<p>"Hello world"</p>" > /var/www/html/index.html
 echo "<p>"Hello world "</p>" > /var/www/index.html
